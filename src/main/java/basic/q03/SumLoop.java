1f4b2f73ac745482437846f5cf8d845ae495e1fd
SHOKO SATO <sh-satou@seassist.co.jp>
2025-02-08 21:04:41 +0900

    first commit

---
+++ src/main/java/basic/q03/SumLoop.java
@@ -0,0 +1,15 @@
+package basic.q03;
+
+public class SumLoop {
+
+	public static void main(String[] args) {
+		int sum = 0;
+		
+		for (int i = 1; i <= 100; i = i + 1) {
+			sum += i;
+		}
+
+			System.out.println(sum);
+	}
+
+}
---
+++ src/main/java/basic/q04/ArayDisplay.java
@@ -0,0 +1,13 @@
+package basic.q04;
+
+public class ArayDisplay {
+
+	public static void main(String[] args) {
+		String fruit[] = {"りんご", "ばなな", "いちご", "みかん", "キウイ"};
+		for (String fav_fruit: fruit) {
+			System.out.println(fav_fruit);
+		}
+
+	}
+
+}
